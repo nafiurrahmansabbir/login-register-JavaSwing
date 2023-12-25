76d562be3de893b655bc93e8d500ee23cf843411
sabbir <you@example.com>
2023-12-25 19:51:34 +0600

    added login page

---
+++ src/main/java/com/mycompany/login/registation/LoginRegistation.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.login.registation;
+
+/**
+ *
+ * @author sabbir
+ */
+public class LoginRegistation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Login obj=new Login();
+        obj.setVisible(true);
+    }
+}
